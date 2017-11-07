8e9d721423de7ed5a77a57d90c608c8d27d21447
Dakota J Pfeifer <dakota.pfeifer@yahoo.com>
2017-11-07 12:40:51 -0700

    added port const in app.js to look for data base and if not present then get local host

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const queries = require('./database/queries')
+const port = process.env.PORT || 3000
 
 const app = express();
 
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
-app.listen(3000, (req,res)=>{
+app.listen(port, (req,res)=>{
   console.log("KEYBLADE!")
 })
 
